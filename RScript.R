9192d5790eac8385ddf95bd7ea99fd3018566f9e
swatisharma1234 <you@example.com>
2019-03-16 00:07:53 -0400

    Code

--- RScript.R
+++ RScript.R
@@ -2,3 +2,14 @@
 # Since data has been loaded to the GitHub project folder
 data<- read.csv("C:/Users/swati/OneDrive - Emory University/Documents/Sem IV/550/MidTerm/Midterm/Data.csv", header = TRUE)
 View(data)
+library(tidyverse)
+library(dplyr)
+colnames(data)
+data2<- data[(complete.cases(data)),]
+nrow(data2)
+data2<- as_tibble(data2)
+
+selectInput("Malnutrition_Type", "Select Malnutrition Status", choices = unique(data2$Moderate.Severe))
+ggplot(data=data2 %>% dplyr::filter(Moderate.Severe==input$Malnutrition_Type),
+       aes(A_PCT, B_PCT)) + 
+  geom_bar(stat =Location )+labs(title="", x="", y="")
