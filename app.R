05a40164e9ca33d0cb7de55987e647d938829871
swatisharma1234 <you@example.com>
2019-03-17 18:41:20 -0400

    Comments Added

--- app.R
+++ app.R
@@ -4,18 +4,19 @@ library(dplyr)
 
 # reading in the dataset
 download.file("https://raw.githubusercontent.com/swatisharma1234/Midterm/master/shinydata.txt", destfile= "Data.csv", mode = "wb")
+#Saving data file as "data1"
 data1 = read.csv("Data.csv", header = TRUE)
-#load("Data.csv")
 data<- data1
 colnames(data)
 
 # Removing missing values form the dataset
 data2<- data[(complete.cases(data)),]
 nrow(data2)
-data2<- as_tibble(data2)
 
-
+# Code for R shiny Server
 server <- function(input, output) {
+# Plot 1 relationship between the child and parent's age, colored by location
+# Filtering can be done by Malnutrition type and gender of child
   
   output$plot_treatment<-renderPlot(
     
@@ -25,7 +26,8 @@ server <- function(input, output) {
       theme(plot.title = element_text(color="#1A5276", face="bold", size=16, hjust=0)) 
     
   )
-  
+# Plot 2 distribution of children in different age groups
+# Filtering can be done by Malnutrition type and gender of child
   output$plot_gender<- renderPlot(ggplot(data=data2 %>% dplyr::filter(Moderate.Severe==input$Malnutrition_Type,sex==input$Gender),
                                          aes(x=reorder(AgeGroup_M,Age_M))) + geom_bar(colour="white",fill="#78A4C0")+
                                     geom_text(stat='count',aes(label=..count..),vjust=-1)+xlab("Age Group of Child")+ylab("Number of Children")+ggtitle("Distribution by Age Groups")+theme_minimal(base_size =12)+
@@ -33,6 +35,7 @@ server <- function(input, output) {
   
 }
 
+# Code for R shiny UI
 ui <- shinyUI(
   fluidPage(
     tags$div(class="header", checked=NA,   tags$h1("Malnutrition and associated factors"),
